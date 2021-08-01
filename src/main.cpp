76ca352d8ce2d77ae52ea777f74c56d2fd6eff5c
erikwastaken <46598911+erikwastaken@users.noreply.github.com>
2021-08-01 17:56:40 +0200

    Get day from user input when not in CLA

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
 #include <memory>
+#include <cstdlib>
 #include "advent_day.h"
 #include "default_day.h"
 
 std::unique_ptr<AdventDay> createDay(int day);
+int parseCommandLineArguments(int argc, char* argv[]);
 
 int main(int argc, char* argv[]) {
-  if (argc < 2) {
-    std::cout << "Provide a day as a command line argument" << std::endl;
-    return 1;
-  }
-  std::cout << "Day" << argv[1] << std::endl;
-  std::string argv1 = argv[1];
-  int num = std::stoi(argv1);
+  int num = parseCommandLineArguments(argc, argv);
   auto day = createDay(num);
+  std::cout << "Day" << num << std::endl;
   std::cout << "Part1: " << day->part1() << std::endl;
   std::cout << "Part2: " << day->part2() << std::endl;
 }
@@ -22,4 +19,18 @@ std::unique_ptr<AdventDay> createDay(int day) {
   switch (day) {
     default: return std::unique_ptr<AdventDay>{ new DefaultDay() };
   }
+}
+
+int getDayNumber() {
+  while (true) {
+    int d {0};
+    std::cout << "Please provide the day: ";
+    std::cin >> d;
+    if (0 < d && d < 26) return d;
+  }
+}
+
+int parseCommandLineArguments(int argc, char* argv[]) {
+  if (argc < 2) return getDayNumber();
+  else return std::atoi(argv[1]);
 }
