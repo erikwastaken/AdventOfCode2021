9e1958f63a91a82f75ce4797445fec2986eee489
erikwastaken <46598911+erikwastaken@users.noreply.github.com>
2021-12-02 07:16:50 +0100

    Remove too strong constraint on const &

--- src/day_handler.cpp
+++ src/day_handler.cpp
@@ -1,9 +1,16 @@
 #include "day_handler.hpp"
+#include "utils.hpp"
+#include "default_day.hpp"
+#include "day01.hpp"
+#include "day02.hpp"
 
 void aoc21::runDay(const int day, const std::string& path) {
   switch (day) {
     case 1: 
       execute(aoc21::Day01(aoc21::getFileContents(path)));
+      break;
+    case 2:
+      execute(aoc21::Day02(aoc21::getFileContents(path)));
       break;
     default:
       execute(aoc21::DefaultDay());
